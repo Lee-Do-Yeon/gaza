de06e227efaca7464824274090011ec74fe4cbfd
leedoyeon <ldy__0202@naver.com>
2023-01-30 16:10:10 +0900

    [BE] feat: reservation list 조회 기능 개발
    각 회원과 가이드가 본인의 페이지에서 예약 내역을 조회할 수 있도록 회원별 예약내역 조회 기능과 가이드별 예약내역 조회 기능을 개발.

--- backend/src/main/java/com/idle/gaza/api/controller/ReservationController.java
+++ backend/src/main/java/com/idle/gaza/api/controller/ReservationController.java
@@ -8,11 +8,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * 예약 관련 API 요청 처리를 위한 컨트롤러 정의.
@@ -26,8 +26,20 @@ public class ReservationController {
     @PostMapping
     @ApiOperation(value = "상담 예약", notes = "특정 가이드에 대해 상담을 예약한다.")
     public ResponseEntity<? extends BaseResponseBody> makeReservation(@RequestBody @ApiParam(value="예약 정보", required = true) ReservationCreatePostRequest reservationInfo){
-        //임의로 리턴된 User 인스턴스. 현재 코드는 회원 가입 성공 여부만 판단하기 때문에 굳이 Insert 된 유저 정보를 응답하지 않음.
-        Reservation reservation = reservationService.createReservation(reservationInfo);
+        reservationService.createReservation(reservationInfo);
         return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
+    }
+
+    @GetMapping("/{userId}")
+    @ApiOperation(value = "예약 내역 리스트 조회(회원)", notes = "회원은 본인의 상담 내역 리스트를 조회 가능하다.")
+    public ResponseEntity<?> getReservationListByUser(@PathVariable @ApiParam(value="예약 정보", required = true) int userId){
+        List<Reservation> reservationList = reservationService.getReservationListByUser(userId);
+        return new ResponseEntity<List<?>>(reservationList, HttpStatus.OK);
+    }
+    @GetMapping("/guide/{guideId}")
+    @ApiOperation(value = "예약 내역 리스트 조회(가이드)", notes = "가이드는 본인의 상담 내역 리스트를 조회 가능하다.")
+    public ResponseEntity<?> getReservationListByGuide(@PathVariable @ApiParam(value="예약 정보", required = true) int guideId){
+        List<Reservation> reservationList = reservationService.getReservationListByGuide(guideId);
+        return new ResponseEntity<List<?>>(reservationList, HttpStatus.OK);
     }
 }
