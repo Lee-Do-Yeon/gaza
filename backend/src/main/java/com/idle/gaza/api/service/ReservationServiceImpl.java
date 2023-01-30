b3a97f61d4dd6a4e6d8fd8bda264725196157287
leedoyeon <gcdalee@gmail.com>
2023-01-30 16:10:10 +0900

    [BE] feat: reservation list 조회 기능 개발
    각 회원과 가이드가 본인의 페이지에서 예약 내역을 조회할 수 있도록 회원별 예약내역 조회 기능과 가이드별 예약내역 조회 기능을 개발.

--- backend/src/main/java/com/idle/gaza/api/service/ReservationServiceImpl.java
+++ backend/src/main/java/com/idle/gaza/api/service/ReservationServiceImpl.java
@@ -1,40 +1,65 @@
 package com.idle.gaza.api.service;
 
 import com.idle.gaza.api.request.ReservationCreatePostRequest;
+import com.idle.gaza.db.entity.Guide;
 import com.idle.gaza.db.entity.Reservation;
+import com.idle.gaza.db.entity.User;
 import com.idle.gaza.db.repository.ReservationRepository;
-import com.idle.gaza.db.repository.ReservationRepositorySupport;
+import com.idle.gaza.db.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 /**
- *	유저 관련 비즈니스 로직 처리를 위한 서비스 구현 정의.
+ *  예약 관련 비즈니스 로직 처리를 위한 서비스 구현 정의.
  */
 @Service("reservationService")
 public class ReservationServiceImpl implements ReservationService {
     @Autowired
     ReservationRepository reservationRepository;
-
     @Autowired
-    ReservationRepositorySupport reservationRepositorySupport;
+    UserRepository userRepository;
 
+    /*
+    @Autowired
+    GuideRepository guideRepository;
+*/
     @Override
     public Reservation createReservation(ReservationCreatePostRequest reservationInfo) {
+        //User user = userRepository.findByUserId(reservationInfo.getUserId());
+        //Guide guide = guideRepository.findByGuideId(reservationInfo.getGuideId());
+
         Reservation reservation = Reservation.builder()
-                .userId(reservationInfo.getUserId())
-                .guideId(reservationInfo.getGuideId())
+                //.userId(user)
+                //.guideId(guide)
                 .consultingDate(reservationInfo.getConsultingDate())
                 .reservationDate(reservationInfo.getReservationDate())
-                .travleStartDate(reservationInfo.getTravleStartDate())
-                .travleEndDate(reservationInfo.getTravleEndDate())
+                .travelStartDate(reservationInfo.getTravelStartDate())
+                .travelEndDate(reservationInfo.getTravelEndDate())
                 .numberOfPeople(reservationInfo.getNumberOfPeople())
                 .withChildren(reservationInfo.getWithChildren())
                 .withElderly(reservationInfo.getWithElderly())
                 .withDisabled(reservationInfo.getWithDisabled())
                 .note(reservationInfo.getNote())
                 .build();
+
         return reservationRepository.save(reservation);
     }
 
+    @Override
+    public void cancelReservation(int reservationId) {
+        reservationRepository.deleteById(reservationId);
+    }
+
+    @Override
+    public List<Reservation> getReservationListByGuide(int guideId) {
+        return reservationRepository.findByGuideId_GuideId(guideId);
+    }
+
+    @Override
+    public List<Reservation> getReservationListByUser(int userId) {
+        return reservationRepository.findByUserId_UserId(userId);
+    }
 
 }
