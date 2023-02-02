76b7141a122d7ef239a9917563b7c88d6a18c07f
이해민 <2haemin11@gmail.com>
2023-02-02 23:03:38 +0900

    [BE]feat:인기 가이드 조회API

--- backend/src/main/java/com/idle/gaza/db/repository/GuideRepository.java
+++ backend/src/main/java/com/idle/gaza/db/repository/GuideRepository.java
@@ -1,7 +1,6 @@
 package com.idle.gaza.db.repository;
 
 import com.idle.gaza.db.entity.Guide;
-import com.idle.gaza.db.entity.User;
 import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -25,5 +24,8 @@ public interface GuideRepository extends JpaRepository<Guide, Integer> {
     /* 해당 회원이 가이드인지 조회 */
     @Query(value = "SELECT *  FROM guide as g WHERE g.user_id=:userId", nativeQuery = true)
     Optional<Guide> findGuideByUser(@Param("userId") int userId);
-    
+
+    /* 예약순 조회 */
+    @Query(value = "select * from guide left join ( select guide_id, count(guide_id) from reservation group by guide_id order by count(*)) reservation on reservation.guide_id=guide.guide_id order by reservation.guide_id", nativeQuery = true)
+    List<Guide> findOrderByMaxReservation();
 }
