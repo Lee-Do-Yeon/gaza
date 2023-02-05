4006297880ce36419e75f51cba828a18fcf1e11f
이해민 <2haemin11@gmail.com>
2023-02-05 10:37:55 +0900

    feat: chat기능구현

--- backend/src/main/java/com/idle/gaza/config/WebSocketConfig.java
+++ backend/src/main/java/com/idle/gaza/config/WebSocketConfig.java
@@ -1,19 +1,25 @@
 package com.idle.gaza.config;
 
 
+import com.idle.gaza.api.handler.ChatHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
-//@Configuration
-//@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Autowired
+    private ChatHandler chatHandler;
+    
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        //해당 endpoint로 handshake가 이루어짐
+        registry.addHandler(chatHandler, "/ws/chat").setAllowedOriginPatterns("*");
     }
-
+    
 }
