a521b3dabda68898b680eb6f88a13b6165316771
S_M_DRAGON <yongsm295@gmail.com>
2023-02-06 15:37:06 +0900

    FE feat: 로그인 로그아웃

--- frontend/common/api/index.js
+++ frontend/common/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 function apiInstance() {
   const instance = axios.create({
-    baseURL: 'https://i8c207.p.ssafy.io',
+    baseURL: 'http://i8c207.p.ssafy.io:8080',
   });
   return instance;
 }
