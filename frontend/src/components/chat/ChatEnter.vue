74773535b529b09ccecd5ebc61e34679bde1ff5d
이해민 <2haemin98@gmail.com>
2023-02-06 12:12:05 +0900

    [FE]fix:merge error

---
+++ frontend/src/components/chat/ChatEnter.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1>채팅방 목록</h1>
+        <form v-on:submit="makeRoom">
+            <input name="title" v-model="roomTitle">
+            <button class="send" type="submit">보내기</button>
+        </form>
+        <ul>
+            <li><a href="/chat/rooms/{{roomId}}"></a></li>
+        </ul>
+
+    </div>
+</template>
+<script>
+import axios from 'axios';
+
+export default{
+
+    data:{
+        roomId:'',
+        roomTitle:'',
+        roomInfo:{}
+    },
+    methods:{
+
+        makeRoom(){
+            axios.post('https://i8c207.p.ssafy.io/chat/room/' + this.roomTitle).then(response=>{this.roomInfo=response.data});
+            console.log(this.roomInfo);
+            this.roomId=this.roomInfo.roomId
+        }
+
+    }
+}
+</script>
