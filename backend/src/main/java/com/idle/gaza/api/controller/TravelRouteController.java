a673e7ff359093daeb53cf53835eeef940422aa7
leedoyeon <ldy__0202@naver.com>
2023-02-06 11:25:10 +0900

    [BE] fix: 여행 일정 관련 오류 수정

--- backend/src/main/java/com/idle/gaza/api/controller/TravelRouteController.java
+++ backend/src/main/java/com/idle/gaza/api/controller/TravelRouteController.java
@@ -1,11 +1,17 @@
 package com.idle.gaza.api.controller;
 
-import com.idle.gaza.api.service.ReviewServiceImpl;
+import com.idle.gaza.api.request.TravelRouteRequest;
+import com.idle.gaza.api.response.TravelRouteResponse;
 import com.idle.gaza.api.service.TravelRouteServiceImpl;
-import io.swagger.annotations.Api;
+import com.idle.gaza.common.model.response.BaseResponseBody;
+import io.swagger.annotations.*;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Api(value = "여행일정 API", tags = {"Travel Route"})
 @RestController
@@ -14,4 +20,26 @@ public class TravelRouteController {
 
     @Autowired
     TravelRouteServiceImpl travelRouteService;
+
+    @PostMapping
+    @ApiOperation(value = "루트 작성", notes = "루트를 저장한다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 500, message = "서버 오류")
+    })
+    public ResponseEntity<? extends BaseResponseBody> saveRoute(@RequestBody @ApiParam(value="루트 정보", required = true) TravelRouteRequest travelInfo){
+        try{
+            travelRouteService.saveRoute(travelInfo);
+        }catch (Exception e){
+            return ResponseEntity.status(500).body(BaseResponseBody.of(500, "fail"));
+        }
+        return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
+    }
+
+    @GetMapping("/{reservationId}")
+    @ApiOperation(value = "여행 일정 조회", notes = "여행 일정을 조회 할 수 있다.")
+    public ResponseEntity<?> getRoutes(@PathVariable int reservationId){
+        List<TravelRouteResponse> routes = travelRouteService.getRoutes(reservationId);
+        return new ResponseEntity<List<?>>(routes, HttpStatus.OK);
+    }
 }
