19f7ee3c52aa635ef86df0b3ee7a79719f7f15b1
leedoyeon <gcdalee@gmail.com>
2023-02-06 11:01:26 +0900

    [BE] fix: 웹소켓 설정 충돌 해결

--- backend/src/main/java/com/idle/gaza/api/controller/MapController.java
+++ backend/src/main/java/com/idle/gaza/api/controller/MapController.java
@@ -1,6 +1,7 @@
 package com.idle.gaza.api.controller;
 import com.idle.gaza.db.entity.MapMessage;
 import com.idle.gaza.db.entity.Point;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
@@ -15,19 +16,19 @@ public class MapController {
 	// 웹소켓으로 들어오는 메시지 발행 처리.
 	// /pub/chat/message로 발행 요청 -> 컨트롤러가 메세지를 받아서 처리
 	// 발행되면 /sub/chat/room/{roomId}로 메세지를 send. 저게 Topic 역할.
-	@MessageMapping("/chat/message")
+	@MessageMapping("/map/message")
 	public void message(@RequestBody MapMessage message) {
 		if (MapMessage.MessageType.ENTER.equals(message.getType()))
 			message.setMessage(message.getSender() + "님이 입장하셨습니다.");
-		messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+		messagingTemplate.convertAndSend("/sub/map/room/" + message.getRoomId(), message);
 	}
 
 	// 웹소켓으로 들어오는 메시지 발행 처리.
 	// /pub/chat/message로 발행 요청 -> 컨트롤러가 메세지를 받아서 처리
 	// 발행되면 /sub/chat/room/{roomId}로 메세지를 send. 저게 Topic 역할.
-	@MessageMapping("/chat/point")
+	@MessageMapping("/map/point")
 	public void point(@RequestBody Point point) {
-		messagingTemplate.convertAndSend("/sub/chat/room2/" + point.getRoomId(), point);
+		messagingTemplate.convertAndSend("/sub/map/room2/" + point.getRoomId(), point);
 	}
 
 	public MapController(SimpMessageSendingOperations messagingTemplate) {
