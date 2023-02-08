dbcd8098de28e97adb9766520b2e487937e31541
최지성 <thsrhzn4@naver.com>
2023-02-08 16:09:23 +0900

    [BE] fix : user

--- backend/src/main/java/com/idle/gaza/db/repository/UserRepository.java
+++ backend/src/main/java/com/idle/gaza/db/repository/UserRepository.java
@@ -14,8 +14,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Integer> {
     @Query(value = "SELECT * FROM user as u WHERE id=:id AND state_code!='US5'", nativeQuery = true)
     Optional<User> login(@Param("id") String id);
-    @Query(value = "SELECT g.* FROM user as u, guide_document as g WHERE g.user_id = u.user_id and u.state_code='US2'", nativeQuery = true)
-    Optional<List<GuideDocument>> searchGuideRegisterList();
     Optional<User> findById(String id);
     Optional<User> findByUserId(int userId);
 }
