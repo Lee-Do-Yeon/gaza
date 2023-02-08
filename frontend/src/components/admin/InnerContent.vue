5d5ee8c4de1b33ecfbc0067ec3ef3d247234c419
최지성 <thsrhzn4@naver.com>
2023-02-08 15:18:35 +0900

    [FE] feature : admin
    관리자 페이지 생성

---
+++ frontend/src/components/admin/InnerContent.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="col-8">
+        <table class="table">
+            <thead class="thead-black">
+                <tr>
+                    <th scope="col">id</th>
+                    <th scope="col">신분증</th>
+                    <th scope="col">체류증빙서류</th>
+                    <th scope="col">자격증</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="user in registerGuideList" :key="user.id"></tr>
+                <tr>
+                    <td> {{user[0]}} </td>
+                    <td> {{user[1]}} </td>
+                    <td> {{user[2]}} </td>
+                    <td> {{user[3]}} </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+import { computed, onMounted } from 'vue';
+import { useStore } from 'vuex';
+
+export default {
+
+
+    setup() {
+        const store = useStore();
+
+        const registerGuideList = computed(() => {
+            return store.getters['adminStore/getRegisterGuideList'];
+        })
+
+        onMounted(() => {
+            store.dispatch['adminStore/getRegisterGuideListAction'];
+        })
+
+        return {
+            store,
+            registerGuideList,
+        };
+  },
+}
+</script>
+
+<style>
+
+</style>
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,6 +4,7 @@ import menuStore from "./menuStore";
 import platformInfoStore from "./platformInfoStore";
 import loadingEffect from "./loadingEffect";
 import createPersistedState from "vuex-persistedstate";
+import adminStore from "./adminStore";
 
 export default createStore({
   modules: {
@@ -11,6 +12,7 @@ export default createStore({
     menuStore,
     platformInfoStore,
     loadingEffect,
+    adminStore
   },
   plugins: [
     createPersistedState({
