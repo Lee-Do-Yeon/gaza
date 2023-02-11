02c3f6e66c132645034f0bf0f8bf747bdb77a1c7
최지성 <thsrhzn4@naver.com>
2023-02-12 02:36:43 +0900

    Update backend/src/main/java/com/idle/gaza/api/controller/UserController.java, backend/src/main/java/com/idle/gaza/common/util/TokenUtil.java

--- backend/src/main/java/com/idle/gaza/common/util/TokenUtil.java
+++ backend/src/main/java/com/idle/gaza/common/util/TokenUtil.java
@@ -240,6 +240,8 @@ public class TokenUtil {
             return Jwts.parserBuilder().setSigningKey(DatatypeConverter.parseBase64Binary(ACCESS_TOKEN_SECRET_KEY)).build()
                     .parseClaimsJws(token).getBody();
         } catch (Exception e) {
+            log.debug("클레임을 가져오는 중 에러 발생");
+
             e.printStackTrace();
 
             return null;
