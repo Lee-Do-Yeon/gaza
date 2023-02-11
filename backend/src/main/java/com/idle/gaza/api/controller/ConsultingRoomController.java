c2c1e512820675c473bcd6037766f771c3de6964
leedoyeon <gcdalee@gmail.com>
2023-02-11 19:50:01 +0900

    [BE] feat: 채팅+맵 소스코드 합침

--- backend/src/main/java/com/idle/gaza/api/controller/ConsultingRoomController.java
+++ backend/src/main/java/com/idle/gaza/api/controller/ConsultingRoomController.java
@@ -2,62 +2,56 @@ package com.idle.gaza.api.controller;
 
 import java.util.List;
 
+import com.idle.gaza.api.model.ChatRoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import com.idle.gaza.db.entity.MapRoom;
-import com.idle.gaza.db.repository.MapRoomRepository;
+import com.idle.gaza.db.entity.ConsultingRoom;
 
 import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/map")
-public class MapRoomController {
+public class ConsultingRoomController {
 
-	private final MapRoomRepository mapRoomRepository;
-
+//	private final MapRoomRepository mapRoomRepository;
+	private final ChatRoomRepository chatRoomRepository;
 	@Autowired
 	private HttpSession httpSession;
 
 	// 모든 채팅방 목록 반환
 	@GetMapping("/rooms")
 	@ResponseBody
-	public List<MapRoom> room() {
-	    return mapRoomRepository.findAllRoom();
+	public List<ConsultingRoom> room() {
+	    return chatRoomRepository.findAllRoom();
 	}
 	 
 	// 채팅방 생성
 	@PostMapping("/room")
 	@ResponseBody
-	public MapRoom createRoom(@RequestBody String name) {
-		MapRoom room = mapRoomRepository.createChatRoom(name);
-	    return room;
+	public ResponseEntity<?> createRoom(@RequestBody String name) {
+		ConsultingRoom room = chatRoomRepository.createRoom(name);
+		if (room == null) return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		return new ResponseEntity<>(room, HttpStatus.OK);
+//	    return room;
 	}
-	 
-	// 채팅방 입장 화면 (x)
-	@GetMapping("/room/enter/{roomId}")
-	public String roomDetail(Model model, @PathVariable String roomId) {
-	    model.addAttribute("roomId", roomId);
-	    return "/map/roomdetail";
-	}
-	 
+
 	 // 특정 채팅방 조회
 	@GetMapping("/room/{roomId}")
 	@ResponseBody
 	public ResponseEntity<?> roomInfo(@PathVariable String roomId) {
 		System.out.println("들어온 룸 아이디 : "+roomId);
-	    MapRoom room = mapRoomRepository.findRoomById(roomId);
-	    System.out.println(room.toString());
+	    ConsultingRoom room = chatRoomRepository.findByRoomId(roomId);
+		if (room == null) return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	    return new ResponseEntity<>(room,HttpStatus.OK);
 	}
 	 
-	public MapRoomController(MapRoomRepository mapRoomRepository) {
+	public ConsultingRoomController(ChatRoomRepository chatRoomRepository) {
 		super();
-		this.mapRoomRepository = mapRoomRepository;
+		this.chatRoomRepository = chatRoomRepository;
 	}
 
 
