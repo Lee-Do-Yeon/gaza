92da6646225dbb082279cec1c7aec1cca62d1ac4
leedoyeon <ldy__0202@naver.com>
2023-02-13 13:30:44 +0900

    [FE] feat : 가이드 사용 언어

--- frontend/src/components/hotel/guideSettings/LanguageBox.vue
+++ frontend/src/components/hotel/guideSettings/LanguageBox.vue
@@ -1,5 +1,8 @@
 <template>
 <div class="col-lg-4 col-md-6 col-sm-12 col-12">
+    <div class="language-list-box">
+        <list-item v-for="(lang,index) in language_list" :key="index" :lang="lang"></list-item>
+    </div>
     <div class="news_item_boxed">
     <form id="profile_form_area" v-on:submit="langRegister">
         <input
@@ -15,18 +18,38 @@
 </template>
 
 <script>
-import { ref } from "vue";
+import { useStore } from "vuex";
+import { onMounted, ref } from "vue";
+import { guideLangRegister, getGuideLang } from "@/../common/api/commonAPI.js";
+import ListItem from "@/components/hotel/guideSettings/ListItem";
 
 export default {
+    components:{
+        ListItem,
+    },
     setup() {
+        const language_list = ref();
+        const store = useStore();
         const language = ref();
+
+        const loginId = store.getters["accountStore/getUserId"];
+
+        //가이드 언어 조회
+        const getLangList = async (loginId) => {
+            const response = await getGuideLang(loginId); //call axios
+            language_list.value = response.data;
+        };
+
+        onMounted(() => {
+            getLangList(loginId);
+        });
 
         //가이드 사용 가능한 언어 등록
         const langRegister = function () {
             const request = language.value;
             const payload = {
             languageName: request,
-            loginId: store.getters["accountStore/getUserId"],
+            loginId: loginId,
             };
 
             guideLangRegister(payload); //call axios
@@ -35,7 +58,17 @@ export default {
         return{
             language,
             langRegister,
+            store,
+            language_list,
         };
     },
 }
 </script>
+
+<style scoped>
+
+.language-list-box {
+    margin-top: 20px;
+}
+
+</style>
---
+++ frontend/src/components/hotel/guideSettings/ListItem.vue
@@ -0,0 +1,32 @@
+<template>
+    <span class="language badge bg-primary">{{ lang.lang_code }}</span><span class="lang-delete">x</span>
+</template>
+
+<script>
+
+export default {
+    props: {
+        index: Number,
+        lang: String,
+    },
+    setup() {
+        
+    },
+}
+</script>
+
+<style scoped>
+
+.language {
+    font-size : 15px;
+    margin-right: 3px;
+}
+.lang-delete {
+    font-size : 15px;
+    font-weight: bold;
+    color : var(--main-color);
+    margin-right: 10px;
+    cursor : pointer;
+}
+
+</style>
