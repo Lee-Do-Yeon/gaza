bfa9dbba83a2b4e7aca8dacd90d9759d411e58fd
이재훈 <dlwogns1193@naver.com>
2023-02-14 11:20:06 +0900

    [FE]feat:review

--- frontend/src/components/hotel/GuideReview.vue
+++ frontend/src/components/hotel/GuideReview.vue
@@ -6,12 +6,12 @@
           <div class="dashboard_menu_area">
             <ul>
               <li>
-                <router-link to="/room-details">내 정보 확인 및 수정</router-link>
+                <router-link to="/room-details"
+                  >내 정보 확인 및 수정</router-link
+                >
               </li>
               <li>
-                <router-link to="/guideInfoDelete"
-                  >내 정보 삭제</router-link
-                >
+                <router-link to="/guideInfoDelete">내 정보 삭제</router-link>
               </li>
               <li>
                 <router-link
@@ -26,45 +26,15 @@
                   :to="{
                     name: 'guide_review',
                   }"
-                  class="active">리뷰조회</router-link
+                  class="active"
+                  >리뷰조회</router-link
                 >
               </li>
             </ul>
           </div>
         </div>
         <div class="col-lg-8">
-          <div class="dashboard_common_table">
-            <div class="dashboard_main_top">
-              <!--
-              <div class="d-flex align-items-center justify-content-between">
-                <div class="col-lg-4 col-md-6 col-sm-12 col-12">
-                  <div class="form_search_date">
-                    <div class="flight_Search_boxed date_flex_area">
-                      <div class="Journey_date">
-                        <p>From</p>
-                        <input type="date" value="2023-01-10" />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-12 col-12">
-                  <div class="form_search_date">
-                    <div class="flight_Search_boxed date_flex_area">
-                      <div class="Journey_date">
-                        <p>To</p>
-                        <input type="date" value="2023-01-30" />
-                      </div>
-                    </div>
-                  </div>
-                </div>
 
-                <div>
-                  <button class="btn btn_theme btn-lg">Filter</button>
-                </div>
-              </div>-->
-            </div>
-          </div>
-          <hr />
 
           <div class="dashboard_common_table">
             <h3>리뷰조회</h3>
@@ -76,14 +46,12 @@
                     <th>작성일</th>
                     <th>별점</th>
                     <th>내용</th>
-                    <!-- <th>Status</th>
-                    <th>Action</th> -->
                   </tr>
                 </thead>
-                <tbody v-for="rev in review" :key="rev.reviewId">
+                <tbody v-for="rev in guidereview" :key="rev.reviewId">
                   <tr>
                     <td>{{ rev.reviewId }}</td>
-                    <td>{{ rev.createdDate }}</td>
+                    <td>{{ getDate(rev.createdDate) }}</td>
                     <td>
                       <i
                         v-for="score in rev.score"
@@ -93,41 +61,11 @@
                       ></i>
                     </td>
                     <td>{{ rev.content }}</td>
-                    <!-- <td class="complete">Completed</td>
-                    <td><i class="fas fa-eye"></i></td> -->
                   </tr>
                 </tbody>
               </table>
             </div>
           </div>
-          <!-- <div class="pagination_area">
-            <ul class="pagination">
-              <li v-if="currentpage !== 1" class="page-item">
-                <a
-                  style="cursor: pointer"
-                  class="page-link"
-                  @click="getValue(currentpage - 1)"
-                  aria-label="Previous"
-                >
-                  <span aria-hidden="true">«</span>
-                  <span class="sr-only">Previous</span>
-                </a>
-              </li>
-              <li  v-for="page in numberofpages" :key="page" class="page-item">
-                <a
-                  style="cursor: pointer"
-                  class="page-link"
-                  @click="getValue(page)"
-                >
-                  {{ page }}</a
-                >
-              </li>
-              <a v-if="currentpage !== numberofpages" style="cursor:pointer" class="page-link" @click="getValue(currentpage +1)" aria-label="Next">
-                <span aria-hidden="true">»</span>
-                <span class="sr-only">Next</span>
-              </a>
-            </ul>
-          </div> -->
         </div>
       </div>
     </div>
@@ -135,29 +73,37 @@
 </template>
 <script>
 import axios from "@/api/http";
-import { mapState } from "vuex";
-const accountStore = "accountStore";
+import { useStore } from "vuex";
+import { onMounted, ref } from "vue";
 
 export default {
   name: "GuideReview",
+  setup() {
+    const guidereview = ref([]);
 
-  data() {
-    return {
-      review: [],
+    const store = useStore();
+    const getDate = (date) => {
+      const DAT = new Date(date);
+      return (
+        DAT.getFullYear() + "-" + (DAT.getMonth() + 1) + "-" + DAT.getDay()
+      );
     };
-  },
-  computed: {
-    ...mapState(accountStore, ["userId"]),
-  },
-  created() {
-    this.reviewList(this.userId);
-  },
-  methods: {
-    reviewList(guideId) {
-      axios.get(`/reviews/guide/${guideId}`).then((res) => {
-        this.review = res.data;
+
+    const getvalue = () => {
+      const loginId = store.getters["accountStore/getUserId"];
+      axios.get(`reviews/guide/id//${loginId}`).then((res) => {
+        guidereview.value = res.data;
       });
-    },
+    };
+
+    onMounted(() => {
+      getvalue();
+    });
+
+    return {
+      guidereview,
+      getDate
+    };
   },
 };
 </script>
